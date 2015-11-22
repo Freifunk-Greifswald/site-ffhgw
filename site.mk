c8eb330462ce19ab10fb25b74e291a04121df0a3
lrnzo <lrnzo@posteo.de>
2015-11-22 15:48:09 +0100

    neue Datei:     i18n/de.mo 	neue Datei:     i18n/de.po 	neue Datei:     i18n/en.mo 	neue Datei:     i18n/en.po 	neue Datei:     i18n/gluon-site.pot 	geändert:       site.conf 	geändert:       site.mk

--- site.mk
+++ site.mk
@@ -3,6 +3,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
+	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
@@ -11,9 +12,11 @@ GLUON_SITE_PACKAGES := \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-legacy \
+	gluon-luci-private-wifi \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
 	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
@@ -23,9 +26,12 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.75~exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+# Languages to include
+GLUON_LANGS ?= en de
 
 GLUON_PRIORITY ?= 0
