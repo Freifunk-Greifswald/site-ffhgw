6aac685aeb1f697770ccf793b65bd091e0a1b363
Frank Weichbrodt <frank.weichbrodt@web.de>
2015-11-27 23:22:37 +0100

    Gluon v2015.1.2

--- site.mk
+++ site.mk
@@ -3,15 +3,14 @@ GLUON_SITE_PACKAGES := \
 	gluon-alfred \
 	gluon-announced \
 	gluon-autoupdater \
-	gluon-config-mode-core \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-legacy \
 	gluon-luci-private-wifi \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
@@ -20,13 +19,13 @@ GLUON_SITE_PACKAGES := \
 	gluon-next-node \
 	gluon-mesh-vpn-fastd \
 	gluon-radvd \
+	gluon-setup-mode \
 	gluon-status-page \
 	iwinfo \
 	iptables \
 	haveged
 
-
-DEFAULT_GLUON_RELEASE := 0.75~exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7.5~$(date +'%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
