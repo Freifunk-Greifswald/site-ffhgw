71742e9b32e590b25cf6edb4da2029fdc634b625
Frank Weichbrodt <frank.weichbrodt@web.de>
2016-07-19 18:03:31 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -26,7 +26,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-authorized-keys \
 	iwinfo
 
-DEFAULT_GLUON_RELEASE := 0.7.7~$(shell date +'%Y-%m-%d')
+DEFAULT_GLUON_RELEASE := 2016.1.5~$(shell date +'%Y-%m-%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
