a1f412ace4ce40fb725a000cd234ac7bfcffa112
BrunoRobert <frank.weichbrodt@web.de>
2017-07-04 18:12:50 +0200

    modifications for v2017.1.1
    new version of ssid-changer

--- site.mk
+++ site.mk
@@ -28,7 +28,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-authorized-keys \
 	iwinfo
 
-DEFAULT_GLUON_RELEASE := 2017-06-14~68edfe0
+DEFAULT_GLUON_RELEASE := yyyy-mm-dd~commit
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
